67e36a335649a1db56c8ee317c5680cba762771e
Olli Meier <hallo@olli-meier.de>
2022-02-11 23:34:09 +0100

    My first Electron App

---
+++ engine/loading_font.py
@@ -0,0 +1,17 @@
+import sys
+from fontTools.ttLib import TTFont
+
+font_path = sys.argv[1]
+
+def get_best_family_name(font_obj):
+    for name_id in [21, 16, 1]:
+        name = font_obj['name'].getDebugName(name_id)
+        if name is not None:
+            return name
+
+font_obj = TTFont(font_path)
+fam_name = get_best_family_name(font_obj)
+
+print(f"fam_name: {fam_name}")
+
+sys.stdout.flush()
---
+++ gui/linkers/fontload.js
@@ -0,0 +1,28 @@
+
+function get_font() {
+    let {PythonShell} = require('python-shell')
+    let path = require("path")
+
+    PythonShell.runString('x=1+1;print(x)', null, function (err) {
+      if (err) throw err;
+      console.log('finished');
+    });
+
+    var font_path = document.getElementById("font_path").value
+    document.getElementById("font_path").value = "";
+
+    let options = {
+        scriptPath : path.join(__dirname, '/../engine/'),
+        pythonPath : '/usr/bin/python3',
+        pythonOptions: ['-u'], // get print results in real-time
+        args : [font_path]
+    }
+
+    let loading_font = new PythonShell('loading_font.py', options);
+
+    loading_font.on('message', function(message){
+        alert(message);
+    })
+}
+
+//document.getElementById("btn_font_path").addEventListener("click", get_font);
